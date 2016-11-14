dd4606f48fa6a84259e20bf154de0114631d5240
XieBing <xxx_bbbb@yeah.net>
2016-11-14 17:19:48 +0800

    test
    用atom测试

--- README.md
+++
@@ -1,2 +0,0 @@
-# Framework
-神舟四号stm32vc107开发板
---
+++ test.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  bool flag;
+
+  cout << "Hello World !" << endl;
+  cin >> flag;
+
+  if (flag) {
+    cout << "true" << endl;
+  } else {
+    cout << "false" << endl;
+  }
+
+  return 0;
+}
